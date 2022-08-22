bc04e8cd033c5a2b0e9962ca76088b9df5be0ff2
BrianLupu93 <brianlupu18@gmail.com>
2022-08-22 11:10:44 +0300

    first test new sticker render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
+import Sticky from "./components/sticky/Sticky.jsx";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [stickers, setStickers] = useState([
+    { title: "brian", body: "programare la doctor", date: "22-22-22" },
+  ]);
+
+  const newSticky = (title, body, date) => {
+    return setStickers({
+      title,
+      body,
+      date,
+    });
+  };
+
+  console.log(stickers);
+  return (
+    <div className="App">
+      {stickers.map((sticker, i) => {
+        return (
+          <Sticky
+            key={i}
+            title={sticker.title}
+            body={sticker.body}
+            date={sticker.date}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
