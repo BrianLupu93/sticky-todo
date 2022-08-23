3f50ac50a8828b740dcd4eff8690164a4724e7ed
BrianLupu93 <brianlupu18@gmail.com>
2022-08-23 08:28:56 +0300

    app.js home

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import { useState } from "react";
 import Sticky from "./components/sticky/Sticky";
 
 function App() {
-  const [stickers, setStickers] = useState([]);
+  const today = new Date();
+  const day = today.getDate();
+  const month = today.getMonth();
+  const year = today.getFullYear();
 
   return (
     <div className="App">
-      <h1 className="app-title">My sticky Notes</h1>
-
-      <InputForm />
-      <Sticky
-        title={"Sticker Title"}
-        body={"This is the body"}
-        date={"22-03-2012"}
-      />
+      <div className="app-header">
+        <h1 className="app-title">My sticky Notes</h1>
+        <h2 className="app-subtitle">Create - Track - Keep</h2>
+        <h4 className="app-subtitle2">YOUR DAILY TASKS</h4>
+      </div>
+      <div className="app-body">
+        <InputForm day={day} month={month} year={year} />
+      </div>
+      <div className="app-footer">
+        <button className="to-stickies-btn">TO MY STICKIES</button>
+      </div>
     </div>
   );
 }
