173967e5664492bfd58abef62a742a6b3cd81e40
BrianLupu93 <brianlupu18@gmail.com>
2022-09-06 12:04:50 +0300

    setState props changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import StickiesBoard from "./components/stickies-board/StickiesBoard";
 function App() {
   // ----------------- GLOBAL STATE ----------------------------------
 
-  const [stickies, setStickies] = useState([]);
+  const [stickies, setStickies] = useState();
 
   const [months, setMonths] = useState([
     { name: "January", number: "01", used: false, count: 0 },
@@ -25,7 +25,7 @@ function App() {
     { name: "December", number: "12", used: false, count: 0 },
   ]);
 
-  // ----------------- DEFAULF STICKY DATA ----------------------------------
+  // ----------------- DEFAULT STICKY DATA ----------------------------------
   const today = new Date();
   const day = today.getDate();
   const month = today.getMonth() + 1;
@@ -33,47 +33,9 @@ function App() {
 
   // ----------------- FUNCTIONS ----------------------------------
 
-  const setData = (data) => setStickies([...stickies, data]);
-
-  const checkMonthName = (sticky) => {
-    const stickyMonth = sticky.month;
-
-    const newMonths = months.map((monthItem) => {
-      if (monthItem.number === stickyMonth && !monthItem.used) {
-        return {
-          ...monthItem,
-          used: !monthItem.used,
-          count: monthItem.count + 1,
-        };
-      }
-      if (monthItem.number === stickyMonth) {
-        return { ...monthItem, count: monthItem.count + 1 };
-      }
-
-      return monthItem;
-    });
-    setMonths(newMonths);
-  };
-
-  const sortStickies = (stickiesList) => {
-    const newStickiesList = stickiesList.sort(
-      (a, b) =>
-        parseInt(a.year) - parseInt(b.year) ||
-        parseInt(a.month) - parseInt(b.month) ||
-        parseInt(a.day) - parseInt(b.day)
-    );
-
-    setStickies(newStickiesList);
-  };
-
   // ----------------- SIDE EFFECTS ----------------------------------
 
-  useEffect(() => {
-    if (stickies.length > 0) {
-      checkMonthName(stickies[stickies.length - 1]);
-      sortStickies(stickies);
-    }
-  }, [stickies]);
+  useEffect(() => {}, [stickies]);
 
   return (
     <BrowserRouter>
@@ -85,7 +47,7 @@ function App() {
               day={day}
               month={month}
               year={year}
-              setStickies={setData}
+              setStickies={setStickies}
               stickies={stickies}
             />
           }
