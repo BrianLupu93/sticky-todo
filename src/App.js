52354a64b6746e0fa9f3b5ef8ac9ffd2743d5bc8
BrianLupu93 <brianlupu18@gmail.com>
2022-09-08 14:00:33 +0300

    months state moved locally

--- src/App.js
+++ src/App.js
@@ -10,21 +10,6 @@ function App() {
 
   const [stickies, setStickies] = useState();
 
-  const [months, setMonths] = useState([
-    { name: "January", number: "01", used: false, count: 0 },
-    { name: "February", number: "02", used: false, count: 0 },
-    { name: "March", number: "03", used: false, count: 0 },
-    { name: "April", number: "04", used: false, count: 0 },
-    { name: "May", number: "05", used: false, count: 0 },
-    { name: "June", number: "06", used: false, count: 0 },
-    { name: "July", number: "07", used: false, count: 0 },
-    { name: "August", number: "08", used: false, count: 0 },
-    { name: "September", number: "09", used: false, count: 0 },
-    { name: "October", number: "10", used: false, count: 0 },
-    { name: "November", number: "11", used: false, count: 0 },
-    { name: "December", number: "12", used: false, count: 0 },
-  ]);
-
   // ----------------- DEFAULT STICKY DATA ----------------------------------
   const today = new Date();
   const day = today.getDate();
