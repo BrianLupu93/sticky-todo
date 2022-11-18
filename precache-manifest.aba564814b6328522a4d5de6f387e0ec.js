5280c2ccc7a32b0bff891415bfe41bd260dca0b2
BrianLupu93 <brianlupu18@gmail.com>
2022-11-18 15:44:53 +0200

    Updates

---
+++ precache-manifest.aba564814b6328522a4d5de6f387e0ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9350ee6273a6bc8f8638",
+    "url": "/static/css/main.39eb1e63.chunk.css"
+  },
+  {
+    "revision": "9350ee6273a6bc8f8638",
+    "url": "/static/js/main.9350ee62.chunk.js"
+  },
+  {
+    "revision": "c8bf16f4e5a7041ed1dd",
+    "url": "/static/js/1.c8bf16f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8d82df47b0847dd60b2806170c46083b",
+    "url": "/static/media/stickyNote.8d82df47.png"
+  },
+  {
+    "revision": "1f325864119185e3e4cc9d06a6d3c8b0",
+    "url": "/index.html"
+  }
+];
